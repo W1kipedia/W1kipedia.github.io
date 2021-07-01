8c44c234923b6fe01bfeb10e2afc5700e9c7c66c
W1kipedia <jrizzo2007@gmail.com>
2021-07-01 15:54:19 -0500

    Created Root react components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from './components/header';
+import { Fragment } from 'react'
+
+const App: React.FC = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
