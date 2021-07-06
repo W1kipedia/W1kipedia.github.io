4fdb112ecb34cc6a47ae046c4c9858fc85a5c17f
W1kipedia <jrizzo2007@gmail.com>
2021-07-05 19:56:53 -0500

    Added a Home Page

---
+++ src/components/home.tsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/home.css';
+
+interface RootProps {};
+
+interface RootState {};
+
+class Root extends Component<RootProps, RootState> {
+	constructor(props: RootProps) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="introduction">
+				<h1>Student, ReactJs beginner, Typescript enthusiast.</h1>
+				<div className="introduction_buttons">
+					<Link to="/about" replace><button>Learn more</button></Link>
+					<Link to="/projects" replace><button>What I've been working on</button></Link>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Root;
